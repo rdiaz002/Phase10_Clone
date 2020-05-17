12f08d112f103c74505484c9bb98c3dc5fb11247
rdiaz002 <r.diazprog@gmail.com>
2020-05-17 18:38:29 -0400

    Updates

---
+++ precache-manifest.3f73ba0c8df03a389ab63e724f063f9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db81fbc31ff46a3f339b4e5a38f3dba9",
+    "url": "/phase10_clone/index.html"
+  },
+  {
+    "revision": "6b14038e75d3db668859",
+    "url": "/phase10_clone/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "c511285a55062fc288cb",
+    "url": "/phase10_clone/static/css/main.008fb34a.chunk.css"
+  },
+  {
+    "revision": "6b14038e75d3db668859",
+    "url": "/phase10_clone/static/js/2.1d13d231.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/phase10_clone/static/js/2.1d13d231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c511285a55062fc288cb",
+    "url": "/phase10_clone/static/js/main.e9da96a9.chunk.js"
+  },
+  {
+    "revision": "7f6eb535cfc1c56e8147",
+    "url": "/phase10_clone/static/js/runtime-main.dd7f6897.js"
+  }
+]);
