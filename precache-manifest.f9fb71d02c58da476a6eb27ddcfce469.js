9fa7fadc6baf7e301af9f6b27ac02a96ef22024a
rdiaz002 <r.diazprog@gmail.com>
2020-05-17 21:23:39 -0400

    Updates

--- precache-manifest.f9fb71d02c58da476a6eb27ddcfce469.js
+++ precache-manifest.f9fb71d02c58da476a6eb27ddcfce469.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "49a9ea87483af07f9ef66cf91cffbacd",
+    "revision": "0c1291b461b8bc2eac2de97f5fbbff7d",
     "url": "/Phase10_Clone/index.html"
   },
   {
-    "revision": "93043c192a78332ec1f8",
+    "revision": "ad7c510ba04970fcbc74",
     "url": "/Phase10_Clone/static/css/2.f42e59c9.chunk.css"
   },
   {
-    "revision": "58a5051b3acc5dc76afa",
+    "revision": "14d2e3ab6665e885e704",
     "url": "/Phase10_Clone/static/css/main.008fb34a.chunk.css"
   },
   {
-    "revision": "93043c192a78332ec1f8",
-    "url": "/Phase10_Clone/static/js/2.6f169ca5.chunk.js"
+    "revision": "ad7c510ba04970fcbc74",
+    "url": "/Phase10_Clone/static/js/2.5bf04e2a.chunk.js"
   },
   {
     "revision": "d0550c944a9928585a31b6efc1a5d942",
-    "url": "/Phase10_Clone/static/js/2.6f169ca5.chunk.js.LICENSE.txt"
+    "url": "/Phase10_Clone/static/js/2.5bf04e2a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "58a5051b3acc5dc76afa",
-    "url": "/Phase10_Clone/static/js/main.6fd1f297.chunk.js"
+    "revision": "14d2e3ab6665e885e704",
+    "url": "/Phase10_Clone/static/js/main.c15bedc1.chunk.js"
   },
   {
     "revision": "82fc0f7ff2a1beb3cb73",
